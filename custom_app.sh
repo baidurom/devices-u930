9dbb2d679b21304532ae0b79cddfa6bc23bc7ce9
duanqizhi01 <duanqizhi01@baidu.com>
2013-08-01 14:32:52 +0800

    [O][Device_Specific] Fix bug of Phone crash. Replace the invoke-interface to invoke-virtual when call method in IccCard. It is not an interface but an abstract class.
    Change-Id: Ia8e757ffe1ca5995cee3547d8266cfc469cea26d

--- custom_app.sh
+++ custom_app.sh
@@ -7,5 +7,8 @@ if [ "$apkBaseName" = "Phone" ];then
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/%%//g'
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/ %[ )\%]/ /g'
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/\(%[0-9]\$[ds]\)%/\1/g'
+
+        # Replace the invoke-interface by invoke-virtual. IccCard is not an Interface but an Abstract Class.
+        find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
 fi
 
