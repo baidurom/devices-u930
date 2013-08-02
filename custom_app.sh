aa7eaf78a44d1d4384ac7d1ef40264d396457541
duanqizhi01 <duanqizhi01@baidu.com>
2013-08-02 16:30:28 +0800

    [R33][O][Device_Specific] Fix crash of Phone when calling. Recreate the SingalInfoToneGenerator after release it.     The error log is :     E/AndroidRuntime: FATAL EXCEPTION: Thread-68     E/AndroidRuntime: java.lang.RuntimeException: Method called after release()     E/AndroidRuntime: 	at android.media.ToneGenerator.stopTone(Native Method)     E/AndroidRuntime: 	at com.android.phone.CallNotifier$SignalInfoTonePlayer.run(CallNotifier.java:2007)
    Change-Id: I73da00d684c22199dedef5393a9675d3b699e6af

--- custom_app.sh
+++ custom_app.sh
@@ -8,6 +8,10 @@ if [ "$apkBaseName" = "Phone" ];then
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/ %[ )\%]/ /g'
 	find $tempSmaliDir -name "*.xml" | xargs sed -i 's/\(%[0-9]\$[ds]\)%/\1/g'
 
+        # Delete the updateCallNotifierRegistrationsAfterRadioTechnologyChange()V method in CallNotifier.smali
+        # We will add a new modified updateCallNotifierRegistrationsAfterRadioTechnologyChange()V method into CallNotifier.smali
+        sed -i -e "/^\.method.*updateCallNotifierRegistrationsAfterRadioTechnologyChange()V/,/^\.end method/d" $tempSmaliDir/smali/com/android/phone/CallNotifier.smali
+
         # Replace the invoke-interface by invoke-virtual. IccCard is not an Interface but an Abstract Class.
         find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
 fi
