296725aa01c24058f714e6312e13892e2a7b2ac5
zoujunhua <zoujunhua@baidu.com>
2013-08-28 16:40:33 +0800

    [R33][O] remove the Baidu Official Verify Preference.
    Change-Id: I178010d4303bd2e610926911aa1b45e2dfe64b73

--- custom_app.sh
+++ custom_app.sh
@@ -25,5 +25,7 @@ elif [ "$apkBaseName" = "Settings" ];then
         # Replace the invoke-interface by invoke-virtual. IccCard is not an Interface but an Abstract Class.
         find $tempSmaliDir -name "*\.smali" | xargs sed -i 's#invoke-interface\(.*Lcom/android/internal/telephony/IccCard;->\)#invoke-virtual\1#g'
 
+        # Remove the Official Verify Preference.
+        sed -i -e "/^\.method.*setOfficialVerifyPreference()V/,/^\.end method/d" $tempSmaliDir/smali/com/android/settings/DeviceInfoSettings.smali
 fi
 
